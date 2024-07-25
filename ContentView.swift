21fc49c9c8ed57e1a14598e38f3ba0e8499b3d85
Ediefalcopuppy <132314572+Ediefalcopuppy@users.noreply.github.com>
2024-07-25 12:23:52 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  everything-calculator
+//
+//  Created by Harry Haverkamp on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Calculator")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
